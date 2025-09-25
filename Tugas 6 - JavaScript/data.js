e6ad6dff7edde7811471b20eabf8471691ad5375
rzldycod <145968979+rzldycod@users.noreply.github.com>
2025-09-25 09:36:42 +0700

    Add files via upload

---
+++ Tugas 6 - JavaScript/data.js
@@ -0,0 +1,14 @@
+const users = [
+  { nama: 'Rogers', umur: 105, alamat: 'Jl. Mawar 1', email: 'rogers1@mail.com' },
+  { nama: 'Stark', umur: 53, alamat: 'Jl. Mawar 2', email: 'stark2@mail.com' },
+  { nama: 'Thor', umur: 1505, alamat: 'Jl. Mawar 3', email: 'thor3@mail.com' },
+  { nama: 'Banner', umur: 53, alamat: 'Jl. Mawar 4', email: 'banner4@mail.com' },
+  { nama: 'Barton', umur: 52, alamat: 'Jl. Mawar 5', email: 'barton5@mail.com' },
+  { nama: 'Romanoff', umur: 39, alamat: 'Jl. Mawar 6', email: 'romanoff6@mail.com' },
+  { nama: 'Wilson', umur: 40, alamat: 'Jl. Mawar 7', email: 'wilson7@mail.com' },
+  { nama: 'Barnes', umur: 101, alamat: 'Jl. Mawar 8', email: 'barnes8@mail.com' },
+  { nama: 'Maximoff', umur: 25, alamat: 'Jl. Mawar 9', email: 'maximoff9@mail.com' },
+  { nama: 'Strange', umur: 42, alamat: 'Jl. Mawar 10', email: 'strange10@mail.com' }
+];
+
+export default users;
